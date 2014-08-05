1a12cf6e85e72945981dbadbc5e502f1560699e1
AndyKr0 <akroner@gmail.com>
2014-08-05 11:58:25 -0400

    grid setup working

--- js/script.js
+++ js/script.js
@@ -1,13 +1,58 @@
-function divGrid(x,y) {
-  for (var i = 0, limit_x = x; i < limit_x; i++){
-    $("#container").append('<div class="grid-element-x">Red</div>');
-    for (var j = 0, limit_y = y; j < limit_y; j++ ){
-      $("#container").append('<div class="grid-element-y">Blue</div>');
+// function divGrid(x,y) {
+//   var i = 0;
+//   $("#container").append('<table>');
+//   //$("table").append('<tr>');
+//   for (i = 1; i < x * y ; i++){
+//
+//
+//     if ((i % y) == 0) or ((i % y) = undefined {
+//       $("table").append('<tr>');
+//
+//     }
+//
+//     $("tr").append('<td><div class="square">' + i + '</div></td>');
+//
+//     if (i >= x) {
+//       if ((i % x) == 0) {
+//         $("table").append('</tr>');
+//       }
+//     }
+//
+//   }
+
+  //$("#table").append('</tr>');
+//   $("#table").append('</table>');
+// }
+
+
+function tableGrid(x,y) {
+
+  var ourTable = '';
+  var i, j = 0;
+
+  ourTable += '<table>';
+  for (var i = 0; i < y ; i++) {
+    ourTable +='<tr>';
+
+    for (var j = 0; j < x; j++) {
+      ourTable +='<td><div class="square">' + i + ',' + j + '</div></td>';
     }
+    ourTable += '</tr>'
   }
+
+
+  ourTable += '</table>'
+
+  $('#main-body').append(ourTable)
+
+
 }
 
 
+
+
+
+
 $(document).ready(function(){
-  divGrid(5,5);
+  tableGrid(5,5);
 });
