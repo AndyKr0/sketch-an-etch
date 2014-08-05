9f2df8fc2103daa72d62a7ed8f9d92412fbe3520
AndyKr0 <akroner@gmail.com>
2014-08-05 12:57:10 -0400

    Initial Grid
    Initial grid color change implemented

--- js/script.js
+++ js/script.js
@@ -1,58 +1,35 @@
-// function divGrid(x,y) {
-//   var i = 0;
-//   $("#container").append('<table>');
-//   //$("table").append('<tr>');
-//   for (i = 1; i < x * y ; i++){
-//
-//
-//     if ((i % y) == 0) or ((i % y) = undefined {
-//       $("table").append('<tr>');
-//
-//     }
-//
-//     $("tr").append('<td><div class="square">' + i + '</div></td>');
-//
-//     if (i >= x) {
-//       if ((i % x) == 0) {
-//         $("table").append('</tr>');
-//       }
-//     }
-//
-//   }
-
-  //$("#table").append('</tr>');
-//   $("#table").append('</table>');
-// }
-
-
 function tableGrid(x,y) {
-
   var ourTable = '';
-  var i, j = 0;
 
   ourTable += '<table>';
   for (var i = 0; i < y ; i++) {
     ourTable +='<tr>';
-
     for (var j = 0; j < x; j++) {
-      ourTable +='<td><div class="square">' + i + ',' + j + '</div></td>';
+      ourTable +='<td><div class="square-default"></div></td>';
     }
     ourTable += '</tr>'
   }
-
-
   ourTable += '</table>'
-
   $('#main-body').append(ourTable)
 
 
 }
 
-
-
+function onHover(color1, color2) {
+   $('.square-default').hover($(this).css({"background-color":"green"}),
+    $(this).css({"background-color":"blue"}));
+}
 
 
 
 $(document).ready(function(){
-  tableGrid(5,5);
+  tableGrid(10,10);
+  $('.square-default').click(function() {
+    $(this).css("background-color","red")});
+  $('.square-default').hover(function() {
+    $(this).css("background-color","green")},
+     function() { $(this).css("background-color","blue")}
+   );
+
+
 });
