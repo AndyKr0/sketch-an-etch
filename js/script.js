f1a620c831419447269a498c4880cf2f499abe50
AndyKr0 <akroner@gmail.com>
2014-08-10 09:57:59 -0400

    version 1.0 - Grid reset and random colors fully implemented

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// FUNCTIONS
+// MAIN
+$(document).ready(function(){
+  //set colors and grid size
+  var colorHover = "yellow";
+  var colorLeave = "green";
+  var colorClick = "grey"
+  var defaultGridSize = 5;
 
+  // logic
+  tableGrid(defaultGridSize, defaultGridSize);
+  mouseEvents(".square-default", colorHover, colorLeave, colorClick);
+
+  $("#random").click(function()
+  {
+
+    mouseEvents(".square-default", randomColorGenerator, randomColorGenerator, colorClick);
+  });
+
+  $('#reset').click(function ()
+  {
+    var gridSize = prompt("Enter grid size:");
+    $('#grid').replaceWith(tableGrid(gridSize, gridSize));
+    mouseEvents(".square-default", colorHover, colorLeave, colorClick);
+  });
+
+});
+
+// FUNCTIONS
 // build a grid x-wide by y-height using a table
 function tableGrid(x,y) {
   var ourTable = '';
@@ -10,10 +36,13 @@ function tableGrid(x,y) {
     for (var j = 0; j < x; j++) {
       ourTable +='<td><div class="square-default"></div></td>';
     }
-    ourTable += '</tr>'
+    ourTable += '</tr>';
   }
-  ourTable += '</table>'
-  $('#main-body').append(ourTable)
+  ourTable += '</table>';
+
+  $('#main-body').append(ourTable);
+  $('.square-default').css('width', (600/x));
+  $('.square-default').css('height', (600/y));
 }
 
 // color changes on hover, mouseclick
@@ -35,25 +64,8 @@ function mouseEvents(element, colorIn, colorOut, colorClick) {
   });
  }
 
-
-// MAIN
-
-$(document).ready(function(){
-  //set colors and grid size
-  var colorHover = "yellow";
-  var colorLeave = "green";
-  var colorClick = "hsl(80, 60%, 70%)"
-  var defaultGridSize = 5;
-
-  // logic
-  tableGrid(defaultGridSize, defaultGridSize);
-  mouseEvents(".square-default", colorHover, colorLeave, colorClick);
-
-  $('#reset').click(function ()
-  {
-    var gridSize = prompt("Enter grid size:");
-    $('#grid').replaceWith(tableGrid(gridSize, gridSize));
-    mouseEvents(".square-default", colorHover, colorLeave, colorClick);
-  });
-
-});
+function randomColorGenerator() {
+  var color = "#" + Math.random().toString(16).slice(2, 8);
+  console.log(color);
+  return color;
+}
